64421c2da7e7ef527cea77dc235b310ce571758d
Ykaro Roberto <orakysec@gmail.com>
2026-06-08 15:05:26 -0300

    fix: add force-dynamic to revalidate route

--- src/app/api/revalidate/route.ts
+++ src/app/api/revalidate/route.ts
@@ -1,6 +1,8 @@
 import { revalidateTag } from "next/cache";
 import { NextResponse } from "next/server";
 
+export const dynamic = "force-dynamic";
+
 export async function GET(request: Request) {
   const { searchParams } = new URL(request.url);
   const secret = searchParams.get("secret");
